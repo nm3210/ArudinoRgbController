46fa3483d510f007a7915f5ce37a601ab55eb81e
nm3210 <nm3210@gmail.com>
2016-08-06 19:10:55 -0400

    Update to 'Revision1'
    This revision incorporates some simple RGB fading based on some code I
    found on stack echange.

--- ArudinoRgbController/ArudinoRgbController.cpp
+++ ArudinoRgbController/ArudinoRgbController.cpp
@@ -1,15 +1,276 @@
 // Do not remove the include below
 #include "ArudinoRgbController.h"
 
+byte ledPinR = 9;
+byte ledPinG = 10;
+byte ledPinB = 11;
 
-//The setup function is called once at startup of the sketch
-void setup()
-{
-// Add your initialization code here
+const int numStates = 9;
+const int alarmState = 0; // this is the state number of when the alarms will go off (see if-statements in loop())
+
+tmElements_t tm;
+
+void setup() {
+    pinMode(A2,OUTPUT);
+    pinMode(A3,OUTPUT);
+    digitalWrite(A2,LOW); // Turn on the ground for the DS1307
+    digitalWrite(A3,HIGH);// Turn on the +5vcc for the DS1307
+
+    // Enable the AIN0 comparator interrupt (called with the ISR(ANALOG_COMP_vect) function below)
+    ACSR = (0 << ACD) |     // Analog Comparator Enabled
+            (0 << ACBG) |   // Analog Comparator Bandgap Select: AIN0 is applied to the positive input
+            (0 << ACO) |    // Analog Comparator Output: Off
+            (1 << ACI) |    // Analog Comparator Interrupt Flag: Clear Pending Interrupt
+            (1 << ACIE) |   // Analog Comparator Interrupt: Enabled
+            (0 << ACIC) |   // Analog Comparator Input Capture: Disabled
+            (1 << ACIS1) | (0 << ACIS0); // Analog Comparator Interrupt Mode: Comparator Interrupt on Falling Output Edge
+
+    // Turn on the serial port
+    Serial.begin(115200);
+    Serial.println("Starting Arduino...");
+
+    // Enable our LED pinouts
+    pinMode(ledPinR, OUTPUT);
+    pinMode(ledPinG, OUTPUT);
+    pinMode(ledPinB, OUTPUT);
+
+    // Get the current time from the DS1307
+    setSyncProvider(RTC.get);
+    if (timeStatus() != timeSet) {
+        Serial.println("RTC: Unable to sync with the RTC, setting time to: ");
+        setTime(00,00,00,01,01,2015);
+    } else {
+        Serial.print("RTC: Successfully set Arduino time to: ");
+    }
+    digitalClockDisplay();
+
+    // Set up our alarms
+    Alarm.alarmRepeat(6, 15, 00, alarm01);  // Turn color to RED
+    Alarm.alarmRepeat(6, 30, 00, alarm02);  // Turn clock to YELLOW
+    Alarm.alarmRepeat(6, 45, 00, alarm03);  // Turn clock to GREEN
+//    Alarm.alarmRepeat(23, 55, 00, alarm03);  // Turn clock to GREEN
+//    Alarm.alarmRepeat(23, 50, 10, alarm03);  // Turn clock to GREEN
+//    Alarm.alarmRepeat(23, 54, 55, alarm04);  // Turn clock to GREEN
+
+    clockCorrect = Alarm.alarmRepeat(dowWednesday, 12, 00, 00, rtcCorrection); // Correct the clock for a bit
+
+    writeRGB(OFF);
 }
 
-// The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+void loop() {
+    if(switchState == alarmState){
+        if(changeState){blinkRGB(OFF);changeState=false;};
+        // Alarm state, leave blank to let the alarms turn on
+//        writeRGB(OFF);
+    } else if (switchState == 1) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+        writeRGB(ON);
+    } else if (switchState == 2) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+//        writeRGB(RED);
+        writeRGB(PINK);
+    } else if (switchState == 3) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+//        writeRGB(GREEN);
+        writeRGB(ORANGE);
+    } else if (switchState == 4) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+//        writeRGB(BLUE);
+        writeRGB(GREEN);
+    } else if (switchState == 5) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+//        crossFadeState = switchState;
+//        if(switchState!=crossFadeState){writeRGB(OFF);return;};
+//        crossFade(OFF,0,0);
+//        crossFade(RED,0,0);
+        writeRGB(BLUE);
+    } else if (switchState == 6) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+//        crossFadeState = switchState;
+//        if(switchState!=crossFadeState){writeRGB(OFF);return;};
+//        crossFade(OFF,0,0);
+//        crossFade(GREEN,0,0);
+        writeRGB(VIOLET);
+    } else if (switchState == 7) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+//        crossFadeState = switchState;
+//        if(switchState!=crossFadeState){writeRGB(OFF);return;};
+//        crossFade(OFF,0,0);
+//        crossFade(BLUE,0,0);
+        crossFadeRGB2(switchState);
+    } else if (switchState == 8) {
+        if(changeState){blinkRGB(OFF);changeState=false;};
+        crossFadeRGB(switchState);
+    }
+
+    digitalClockDisplay();
+    Alarm.delay(100);
+}
+
+void alarm01(){
+	Serial.println("alarm01");
+    if(switchState!=alarmState){return;}
+    crossFadeState = switchState;
+    crossFade(RED,3);
+}
+
+void alarm02(){
+	Serial.println("alarm02");
+    if(switchState!=alarmState){return;}
+    crossFadeState = switchState;
+    crossFade(YELLOW,3);
+}
+
+void alarm03(){
+	Serial.println("alarm03");
+    if(switchState!=alarmState){return;}
+    crossFadeState = switchState;
+    crossFade(GREEN,3);
+}
+
+void rtcCorrection(){
+	digitalClockDisplay();
+	Serial.print("rtcCorrection: ");
+    if(switchState!=alarmState){return;}
+	// Disable the current alarm
+//	Alarm.disable(clockCorrect);
+	// Correct the clock
+	long singleWeekDelta = (long) -offsetSecPerYear / 365 * 7;
+	Serial.print(singleWeekDelta);
+    adjustTime(singleWeekDelta);
+    RTC.set(now());
+    // Wait until AFTER our alarm is going to start again to enable it
+//	Serial.print(" & wait for ");
+//	Serial.print(singleDayDelay*110);
+//    Alarm.delay(singleDayDelay*110);
+//    Alarm.enable(clockCorrect);
+    Serial.println(". Done.");
+    digitalClockDisplay();
+}
+
+
+void digitalClockDisplay(){
+    // digital clock display of the time
+    Serial.print(year());
+    Serial.print("-");
+    print2digits(month());
+    Serial.print("-");
+    print2digits(day());
+    Serial.print(" ");
+    print2digits(hour());
+    Serial.print(":");
+    print2digits(minute());
+    Serial.print(":");
+    print2digits(second());
+    Serial.println();
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
+
+void crossFadeRGB(int curState){
+    crossFadeState = curState;
+    if(switchState!=crossFadeState){writeRGB(OFF);return;};
+    crossFade(RED, 0, 0);
+    crossFade(YELLOW, 0, 0);
+    crossFade(GREEN, 0, 0);
+    crossFade(CYAN, 0, 0);
+    crossFade(BLUE, 0, 0);
+    crossFade(MAGENTA, 0, 0);
+}
+
+void crossFadeRGB2(int curState){
+    crossFadeState = curState;
+    if(switchState!=crossFadeState){writeRGB(OFF);return;};
+    crossFade(BLUE, 3, 0);
+    crossFade(CYAN, 3, 0);
+    crossFade(GREEN, 3, 0);
+    crossFade(CYAN, 3, 0);
+}
+
+// Function for the AIN0 interrupt; need to keep all the variables as volatile, can't really call other functions
+ISR(ANALOG_COMP_vect) {
+    buttonCurTime = millis();
+    //check to see if the interrupt was called in the last 250 milliseconds
+    if (abs(buttonCurTime - buttonLastTime) > 250) {
+        buttonLastTime = buttonCurTime;
+        // Call our interrupt function
+        btnInterrupt();
+    }
+}
+
+void btnInterrupt() {
+    count++;
+    switchState = count % numStates;
+    changeState = true;
+}
+
+void writeRGB(int r, int g, int b) {
+    // Clamp the values to be between the min/max
+//    r = max(min(r,255), 0);
+//    g = max(min(g,255), 0);
+//    b = max(min(b,255), 0);
+
+    redVal = r;
+    grnVal = g;
+    bluVal = b;
+
+    // Write the pin values out (this assumes high is off, and low is on);
+    analogWrite(ledPinR, 255-r);
+    analogWrite(ledPinG, 255-g);
+    analogWrite(ledPinB, 255-b);
+}
+
+int calculateStep(int prevValue, int endValue) {
+    int step = endValue - prevValue; // What's the overall gap?
+    if (step) {                      // If its non-zero,
+        step = 1020 / step;              //   divide by 1020
+    }
+    return step;
+}
+int calculateVal(int step, int val, int i) {
+    if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+        if (step > 0) {          //   increment the value if step is positive...
+            val += 1;
+        } else if (step < 0) {       //   ...or decrement it if step is negative
+            val -= 1;
+        }
+    }
+    // Defensive driving: make sure val stays in the range 0-255
+    return min(max(val,0), 255);
+}
+void crossFade(Color c) {
+    if(switchState!=crossFadeState){writeRGB(OFF);return;}; // Bail out
+    int stepR = calculateStep(prevR, c.red);
+    int stepG = calculateStep(prevG, c.green);
+    int stepB = calculateStep(prevB, c.blue);
+
+    for (int i = 0; i <= 1020; i++) {
+        if(switchState!=crossFadeState){writeRGB(OFF);return;}; // Bail out if we set our RAM flag
+
+        redVal = calculateVal(stepR, redVal, i);
+        grnVal = calculateVal(stepG, grnVal, i);
+        bluVal = calculateVal(stepB, bluVal, i);
+
+        writeRGB(redVal,grnVal,bluVal);
+
+        Alarm.delay(wait); // Pause for 'wait' milliseconds before resuming the loop
+    }
+
+    // Update current values for next loop
+    prevR = redVal;
+    prevG = grnVal;
+    prevB = bluVal;
+    Alarm.delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
+}
+
+void blinkRGB(Color c){
+    writeRGB(c);
+    Alarm.delay(50);
+    writeRGB(OFF);
+    Alarm.delay(50);
 }
--- ArudinoRgbController/ArudinoRgbController.h
+++ ArudinoRgbController/ArudinoRgbController.h
@@ -7,16 +7,126 @@
 #ifndef _ArudinoRgbController_H_
 #define _ArudinoRgbController_H_
 #include "Arduino.h"
-//add your includes for the project ArudinoRgbController here
-
+//add your includes for the project Testing01 here
+#include "Time.h"
+#include "TimeAlarms.h"
+#include "DS1307RTC.h"
 
 //end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
 
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
-//add your function definitions for the project ArudinoRgbController here
+//add your function definitions for the project Testing01 here
+int crossFadeState;
+int wait = 10;      // 10ms internal crossFade delay; increase for slower fades
+int hold = 0; // Optional hold when a color is complete, before the next crossFade
 
+// RTC Correction Values
+//const signed int secPerYear = 4368;
+const signed int offsetSecPerYear = 5012;
+AlarmID_t clockCorrect;
 
+// RAM-specific variables to keep track within the interrupts
+volatile unsigned long buttonCurTime = 0;
+volatile unsigned long buttonLastTime = 0;
+volatile int count = 0;
+volatile int switchState = 0;
+volatile boolean changeState = false;
 
+int redVal = 0;
+int grnVal = 0;
+int bluVal = 0;
+int prevR = 0;
+int prevG = 0;
+int prevB = 0;
 
+const double expConvert = 0.0217457938712615;
+
+struct Color {
+    const uint8_t red, green, blue;
+    Color(uint8_t r, uint8_t g, uint8_t b):
+            red(r), green(g), blue(b) {
+    }
+};
+
+static const Color RED     (255,   0,   0);
+static const Color GREEN   (  0, 255,   0);
+static const Color BLUE    (  0,   0, 255);
+
+static const Color MAGENTA (255,   0, 255);
+static const Color YELLOW  (255, 150,   0);
+static const Color CYAN    (  0, 255, 255);
+
+static const Color ORANGE  ( 83,   4, 000);
+static const Color INDIGO  ( 10,   0,  50);
+static const Color VIOLET  ( 20,   0, 100);
+static const Color PINK    (158,   4,  79);
+
+static const Color WHITE   (255, 255, 255);
+static const Color DIM     ( 20,  20,  20);
+static const Color BLACK   (  0,   0,   0);
+static const Color ON = WHITE;
+static const Color OFF = BLACK;
+static const Color PURPLE = VIOLET;
+
+void alarm01();
+void alarm02();
+void alarm03();
+void rtcCorrection();
+void digitalClockDisplay();
+void print2digits(int digits);
+void btnInterrupt();
+void crossFadeRGB(int g);
+void crossFadeRGB2(int g);
+void writeRGB(Color c);
+void writeRGB(Color c, int d);
+void writeRGB(int r, int g, int b);
+void writeRGB(int r, int g, int b, int d);
+void crossFade(Color c);
+void crossFade(Color c, int w);
+void crossFade(Color c, int w, int d);
+void blinkRGB(Color c);
+void blinkRGB(Color c, int count);
+int convertColor(int c);
+
+void writeRGB(Color c){
+    writeRGB(c.red,c.green,c.blue);
+}
+void writeRGB(int r, int g, int b, int d) {
+    writeRGB(r, g, b);
+    Alarm.delay(d);
+}
+void writeRGB(Color c, int d) {
+    writeRGB(c);
+    Alarm.delay(d);
+}
+
+void crossFade(Color c, int w) {
+    int oldwait = wait;
+    wait = w;
+    crossFade(c);
+    wait = oldwait;
+}
+void crossFade(Color c, int w, int d) {
+    int oldhold = hold;
+    hold = d;
+    crossFade(c, w);
+    hold = oldhold;
+}
+void blinkRGB(Color c, int count){
+    for(int i=0;i<count;i++){
+        blinkRGB(c);
+    }
+}
+int convertColor(int c){
+	return ceil((double) exp(c * expConvert) - 1);
+}
 //Do not add code below this line
 #endif /* _ArudinoRgbController_H_ */
