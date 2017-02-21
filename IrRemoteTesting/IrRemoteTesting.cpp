805a1696bab5c05d3927bf45f6fef2127410543c
nm3210 <nm3210@gmail.com>
2017-02-20 22:42:14 -0500

    Add more IR button lookups
    This adds a bunch more buttons from various remotes.

--- IrRemoteTesting/IrRemoteTesting.cpp
+++ IrRemoteTesting/IrRemoteTesting.cpp
@@ -122,81 +122,81 @@ bool decodeIrSignal(){
             changeModes = false;
             break;
 
-        case BTNCTRL1_1:
+        case CTRL1BTN_1:
             Serial.println("Button 1");
             curMode = 0;
             break;
 
-        case BTNCTRL1_2:
+        case CTRL1BTN_2:
             Serial.println("Button 2");
             curMode = 1;
             break;
 
-        case BTNCTRL1_3:
+        case CTRL1BTN_3:
             Serial.println("Button 3");
             curMode = 2;
             break;
 
-        case BTNCTRL1_4:
+        case CTRL1BTN_4:
             Serial.println("Button 4");
             curMode = 3;
             break;
 
-        case BTNCTRL1_5:
+        case CTRL1BTN_5:
             Serial.println("Button 5");
             curMode = 4;
             break;
 
-        case BTNCTRL1_6:
+        case CTRL1BTN_6:
             Serial.println("Button 6");
             curMode = 5;
             break;
 
-        case BTNCTRL1_7:
+        case CTRL1BTN_7:
             Serial.println("Button 7");
             curMode = 6;
             break;
 
-        case BTNCTRL1_8:
+        case CTRL1BTN_8:
             Serial.println("Button 8");
             curMode = 7;
             break;
 
-        case BTNCTRL1_9:
+        case CTRL1BTN_9:
             Serial.println("Button 9");
             curMode = 8;
             break;
 
-        case BTNCTRL1_0:
+        case CTRL1BTN_0:
             Serial.println("Button 0");
             curMode = 9;
             break;
 
-        case BTNCTRL1_ASTRX:
+        case CTRL1BTN_ASTRX:
             Serial.println("Button ASTERIX");
             break;
 
-        case BTNCTRL1_POUND:
+        case CTRL1BTN_POUND:
             Serial.println("Button POUND");
             break;
 
-        case BTNCTRL1_SELECT:
+        case CTRL1BTN_SELECT:
             Serial.println("Button OK");
             break;
 
-        case BTNCTRL1_UP:
+        case CTRL1BTN_UP:
             Serial.println("Button UP");
             break;
 
-        case BTNCTRL1_DOWN:
+        case CTRL1BTN_DOWN:
             Serial.println("Button DOWN");
             break;
 
-        case BTNCTRL1_LEFT:
+        case CTRL1BTN_LEFT:
             Serial.println("Button LEFT");
             break;
 
-        case BTNCTRL1_RIGHT:
+        case CTRL1BTN_RIGHT:
             Serial.println("Button RIGHT");
             break;
         }
