e33c51e23b12b1d101a087e69e6f3993a4fb26cc
nm3210 <nm3210@gmail.com>
2017-03-08 11:24:29 -0500

    Testing putting multiple data structures
    This tests multiple data array structures and some test data.

--- EepromTesting/EepromTesting.cpp
+++ EepromTesting/EepromTesting.cpp
@@ -1,35 +1,53 @@
 // Do not remove the include below
 #include "EepromTesting.h"
 
+const uint8_t  numSegments = 3;
+SegmentData segmentData[numSegments];
+
 void setup() {
     // Turn on serial port
     Serial.begin(9600);
     while(!Serial){}; // Wait for serial to connect
     Serial.println("Starting Arduino...");
 
+    // Initialize segment data
+    for(uint8_t i=0; i < numSegments; i++){
+        segmentData[i].initialize();
+
+        // Fill in the data with some data to test reading
+        segmentData[i].brightness =+ i;                          // 1,2,3
+        segmentData[i].saturation = segmentData[i].saturation-i; // 15,14,13
+        segmentData[i].curMode    = static_cast<LightMode>(i+1); // 1,2,3
+        segmentData[i].prevMode   = static_cast<LightMode>(i+5); // 5,6,7
+    }
+
+
     int eeAddress = 0;
-    SegmentData segDataWrite;
-    segDataWrite.initialize();
-    EEPROM.put(eeAddress, segDataWrite);
-    Serial.print("Wrote SegmentData type!");
+    EEPROM.put(eeAddress, segmentData);
+    Serial.println("Wrote SegmentData type!");
 }
 
 void loop() {
     int eeAddress = 0;
-    SegmentData segDataRead;
-    EEPROM.get(eeAddress, segDataRead);
+    EEPROM.get(eeAddress, segmentData);
 
     Serial.print("Read SegmentData [sizeof(SegmentData)==");
     Serial.print(sizeof(SegmentData));
-    Serial.print("bytes, sizeof(segDataRead)==");
-    Serial.print(sizeof(segDataRead));
-    Serial.print("bytes] brightness=");
-    Serial.print(segDataRead.brightness);
-    Serial.print(", saturation=");
-    Serial.print(segDataRead.saturation);
-    Serial.print(", curMode=");
-    Serial.print(segDataRead.curMode);
-    Serial.print(", prevMode=");
-    Serial.println(segDataRead.prevMode);
+    Serial.print("bytes, sizeof(segmentData)==");
+    Serial.print(sizeof(segmentData));
+    Serial.println("bytes]: ");
+
+    for(uint8_t i=0; i < numSegments; i++){
+        Serial.print("    item #");
+        Serial.print(i);
+        Serial.print(": brightness=");
+        Serial.print(segmentData[i].brightness);
+        Serial.print(", saturation=");
+        Serial.print(segmentData[i].saturation);
+        Serial.print(", curMode=");
+        Serial.print(segmentData[i].curMode);
+        Serial.print(", prevMode=");
+        Serial.println(segmentData[i].prevMode);
+    }
     delay(1000);
 }
